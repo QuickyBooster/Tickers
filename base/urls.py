c5c667b74908d718508ddae20e66e1cfd9915816
QuickyBooster <quicklybooster.vh@gmail.com>
2024-03-26 16:18:21 +0700

    done navbar

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("/profile", views.profile, name="profile"),
+    path("profile", views.profile, name="profile"),
     # path('/my-ticket', views.my_ticket,name='ticket'),
     # path('/payment', views.payment,name='payment'),
     # path('/event', views.event,name='event'),
