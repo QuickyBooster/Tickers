cd2ed6628029fb71ce795765ac9668d7a35b952f
RosDeeper <hoanggg237@gmail.com>
2024-03-28 02:05:35 +0700

    myticket-incoming and myticket-short (html)

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "payment.html")
+    return render(request, "myticket-incoming.html")
 
 
 def profile(request):
